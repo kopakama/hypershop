f433460ebcdcfce8bbd670ab24ed362bf8a8ffcb
Chris Lee <simakov-art@mail.ru>
2020-08-17 23:49:46 +0700

    added video block to reviews

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ $(function() {
   });
 
   $(document).ready(function(){
-    $('.gallery').slick({
+    $('.gallery--photo').slick({
       infinite: true,
       slidesToShow: 3,
       slidesToScroll: 3,
@@ -14,3 +14,15 @@ $(function() {
       dots: true
     });
   });
+
+  $(document).ready(function(){
+    $('.gallery--video').slick({
+      infinite: true,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      arrows: true,
+      dots: true
+    });
+  });
+
+  
