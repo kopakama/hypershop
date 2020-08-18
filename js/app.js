61e3772117463ccddb1eada5b0414178add5b3e2
Chris Lee <simakov-art@mail.ru>
2020-08-18 23:26:50 +0700

    9th block form

--- js/app.js
+++ js/app.js
@@ -1,42 +1,49 @@
 $(function() {
-    $("#phone").mask("+7 (000) 000-00-00", {
-      placeholder: "+7(___)___-__-__",
-      clearIfNotMatch: true
-    });
+  $("#phone").mask("+7 (000) 000-00-00", {
+    placeholder: "+7(___)___-__-__",
+    clearIfNotMatch: true
   });
+});
 
-  $(document).ready(function(){
-    $('.gallery--photo').slick({
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 3,
-      arrows: true,
-      dots: true
-    });
+$(function() {
+  $("#phone2").mask("+7 (000) 000-00-00", {
+    placeholder: "+7(___)___-__-__",
+    clearIfNotMatch: true
   });
+});
 
-  $(document).ready(function(){
-    $('.gallery--video').slick({
-      infinite: true,
-      slidesToShow: 2,
-      slidesToScroll: 2,
-      arrows: true,
-      dots: true
-    });
+$(document).ready(function(){
+  $('.gallery--photo').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: true,
+    dots: true
+  });
+});
+
+$(document).ready(function(){
+  $('.gallery--video').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    arrows: true,
+    dots: true
+  });
+});
+
 
     /* Collapse */
-    $("[data-collapse]").on("click", function(event){
-      event.preventDefault();
+$("[data-collapse]").on("click", function(event){
+  event.preventDefault();
 
-      var $this = $(this)
-          blockId = $this.data('collapse');
+  var $this = $(this)
+      blockId = $this.data('collapse');
 
-      
-      $(blockId).slideToggle(100, function(){
-        $this.toggleClass("active");
-      });
-      
-    });
+  
+  $(blockId).slideToggle(100, function(){
+    $this.toggleClass("active");
   });
+});
 
   
