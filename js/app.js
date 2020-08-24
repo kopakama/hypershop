9a4b6b0a8b7777a215ab1fa3deb42bb494f59f60
Chris Lee <simakov-art@mail.ru>
2020-08-24 23:07:04 +0700

    adaptive completed

--- js/app.js
+++ js/app.js
@@ -12,28 +12,122 @@ $(function() {
   });
 });
 
-$(document).ready(function(){
-  $('.gallery--photo').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    arrows: true,
-    dots: true
-  });
+//slider
+
+$('.gallery--photo').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  arrows: true,
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 1135,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true,
+        //autoplay: true
+      }
+    },
+    {
+      breakpoint: 856,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        //autoplay: true
+      }
+    },
+    {
+      breakpoint: 420,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        arrows: false
+        //autoplay: true
+      }
+    },
+
+  ]
 });
 
-$(document).ready(function(){
-  $('.gallery--video').slick({
-    infinite: true,
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    arrows: true,
-    dots: true
-  });
+
+$('.gallery--video').slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  arrows: true,
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 856,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        //autoplay: true
+      }
+    },
+    {
+      breakpoint: 420,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        arrows: false,
+        autoplay: true
+      }
+    },
+  ]
+});
+
+$('.easy-cards').slick({
+  slidesToShow: 4,
+
+  responsive: [
+    {
+      breakpoint: 1041,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true,
+        //autoplay: true
+      }
+    },
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        //autoplay: true
+      }
+    },
+    {
+      breakpoint: 370,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        arrows: false,
+        autoplay: true
+      }
+    },
+  ]
 });
 
 
-    /* Collapse */
+/* Collapse */
 $("[data-collapse]").on("click", function(event){
   event.preventDefault();
 
